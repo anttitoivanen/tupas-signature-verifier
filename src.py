4540da60e545791d19c33f59353524b8d3a39845
Antti Toivanen <atoivane@gmail.com>
2019-02-19 15:49:21 +0200

    Fix the tests and move the testing secrets to a more sensible location

--- src.py
+++ src.py
@@ -1,9 +1,6 @@
 import hashlib
 import urllib.parse
 
-INPUT_SECRET = 'inputsecret' #very scret
-OUTPUT_SECRET = 'outputsecret'
-
 def get_params(url):
 	qstring = url.split('?')[1]
 	params = [param.split('=') for param in qstring.split('&')]
@@ -15,32 +12,40 @@ def urldecode(string):
 def urlencode(string):
 	return urllib.parse.quote(string, encoding='windows-1252')
 
-def make_querystring(params):
-	attr_names = ['B02K_VERS', 'B02K_TIMESTMP', 'B02K_IDNBR', 'B02K_STAMP', 'B02K_CUSTNAME', 'B02K_KEYVERS', 'B02K_ALG', 'B02K_CUSTID', 'B02K_CUSTTYPE']
+def make_querystring(params, secret):
+	attr_names = ['B02K_VERS', 
+								'B02K_TIMESTMP', 
+								'B02K_IDNBR', 
+								'B02K_STAMP', 
+								'B02K_CUSTNAME', 
+								'B02K_KEYVERS', 
+								'B02K_ALG', 
+								'B02K_CUSTID', 
+								'B02K_CUSTTYPE']
 	parts = [urldecode(params[attr]) for attr in attr_names]
-	return f"{'&'.join(parts)}&{INPUT_SECRET}&"
+	return f"{'&'.join(parts)}&{secret}&"
 
 def make_error(server, err):
 	return f"{server}?error={urlencode(err)}"
 
-def make_output(server, cust_name):
-	name = [part.title() for part in cust_name.split('%20')]
-	hash_source = f'firstname={name[0]}&lastname={name[1]}#{OUTPUT_SECRET}'
+def make_output(server, cust_name, secret):
+	name = [part.capitalize() for part in cust_name.split('%20')]
+	hash_source = f'firstname={name[0]}&lastname={name[1]}#{secret}'
 	hashed = hashlib.sha256(hash_source.encode('utf-8')).hexdigest()
-	return f"{server}?{hash_source.split('#')[0]}&hash={hashed}"
+	return f"{server}?firstname={name[0]}&lastname={name[1]}&hash={hashed}"
 
-def validate(url):
+def validate(url, input_secret, output_secret):
 	try:
 		server = url.split('?')[0]
 		params = get_params(url)
 	except:
 		return make_error(server, 'Malformed URL')
 	try:
-		querystring = make_querystring(params)
+		querystring = make_querystring(params, input_secret)
 	except KeyError as err:
 		return make_error(server, f"Missing parameter: {err}")
 	signature = hashlib.sha256(querystring.encode('utf-8')).hexdigest()
 	if not params['B02K_MAC'] or signature.upper() != params['B02K_MAC']:
 		return make_error(server, 'Incorrect signature')
 	else:
-		return make_output(server, params['B02K_CUSTNAME'])
+		return make_output(server, params['B02K_CUSTNAME'], output_secret)
--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import unittest
 from src import validate, urldecode
 
+INPUT_SECRET = 'inputsecret' #very scret
+OUTPUT_SECRET = 'outputsecret'
+
 class TestValidation(unittest.TestCase):
 
 	def test_decode(self):
@@ -10,26 +13,27 @@ class TestValidation(unittest.TestCase):
 	def test_simple(self):
 		url = 'http://someserver.com/?B02K_VERS=0003&B02K_TIMESTMP=50020181017141433899056&B02K_IDNBR=2512408990&B02K_STAMP=20010125140015123456&B02K_CUSTNAME=FIRST%20LAST&B02K_KEYVERS=0001&B02K_ALG=03&B02K_CUSTID=9984&B02K_CUSTTYPE=02&B02K_MAC=EBA959A76B87AE8996849E7C0C08D4AC44B053183BE12C0DAC2AD0C86F9F2542'
 		excpected_result = 'http://someserver.com/?firstname=First&lastname=Last&hash=4f6536ca2a23592d9037a4707bb44980b9bd2d4250fc1c833812068ccb000712'
-		self.assertEqual(validate(url), excpected_result)
+		self.assertEqual(validate(url, INPUT_SECRET, OUTPUT_SECRET), excpected_result)
 
 	def test_vaino(self):
 		url = 'http://someserver.com/?B02K_VERS=0003&B02K_TIMESTMP=50020181017141433899056&B02K_IDNBR=2512408990&B02K_STAMP=20010125140015123456&B02K_CUSTNAME=V%C4IN%D6%20M%C4KI&B02K_KEYVERS=0001&B02K_ALG=03&B02K_CUSTID=9984&B02K_CUSTTYPE=02&B02K_MAC=D097F084296A5504BF8AEFA839B5BEC4453F5EFAD67FCB543C0EE554824151EF'
-		print(validate(url))
+		excpected_result = 'http://someserver.com/?firstname=V%c4in%d6&lastname=M%c4ki&hash=c6c6a51a460085e1ff15f988d0ba4a83b433e10356af3bac943b751cc2f23f92'
+		self.assertEqual(validate(url, INPUT_SECRET, OUTPUT_SECRET), excpected_result)
 
 	def test_invalid_url(self):
 		fakeurl = 'foobar'
 		excpected_result = 'foobar?error=Malformed%20URL'
-		self.assertEqual(validate(fakeurl), excpected_result)
+		self.assertEqual(validate(fakeurl, INPUT_SECRET, OUTPUT_SECRET), excpected_result)
 
 	def test_missing_parameter(self):
 		url = 'http://someserver.com/?B02K_TIMESTMP=50020181017141433899056&B02K_IDNBR=2512408990&B02K_STAMP=20010125140015123456&B02K_CUSTNAME=FIRST%20LAST&B02K_KEYVERS=0001&B02K_ALG=03&B02K_CUSTID=9984&B02K_CUSTTYPE=02&B02K_MAC=EBA959A76B87AE8996849E7C0C08D4AC44B053183BE12C0DAC2AD0C86F9F2542'
 		excpected_result = 'http://someserver.com/?error=Missing%20parameter%3A%20%27B02K_VERS%27'
-		self.assertEqual(validate(url), excpected_result)
+		self.assertEqual(validate(url, INPUT_SECRET, OUTPUT_SECRET), excpected_result)
 
 	def test_invalid_signature(self):
 		url = 'http://someserver.com/?B02K_VERS=0003&B02K_TIMESTMP=50020181017141433899056&B02K_IDNBR=2512408990&B02K_STAMP=20010125140015123456&B02K_CUSTNAME=FIRST%20LAST&B02K_KEYVERS=0001&B02K_ALG=03&B02K_CUSTID=9984&B02K_CUSTTYPE=02&B02K_MAC=EBA959A76B87AE8996849E7C0C08D4AC44B053183BE12C0DAC2AD0C86F9F2543'
 		excpected_result = 'http://someserver.com/?error=Incorrect%20signature'
-		self.assertEqual(validate(url), excpected_result)
+		self.assertEqual(validate(url, INPUT_SECRET, OUTPUT_SECRET), excpected_result)
 
 
 if __name__ == '__main__':
